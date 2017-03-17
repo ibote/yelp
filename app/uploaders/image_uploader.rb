b5e3cd9de114dbd226a3a6b3311958eaffee7e9b
Wolfgang Wick <wolfgang.wick@ibote.de>
2017-03-17 11:57:48 +0100

    image_uploader repair

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.envproduction?
+  if Rails.env.production?
     storage :fog
   else
     storage :file
