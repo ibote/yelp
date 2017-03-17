71c1f75465324a14a1fa704396dc9ec0fbb5a97d
Wolfgang Wick <wolfgang.wick@ibote.de>
2017-03-17 17:03:42 +0100

    BUG

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/google'                        # required
   config.fog_credentials = {
     provider:                         'Google',
-    google_storage_access_key_id:     'GOOGZ4XSD2LD7W6LX4BM',
-    google_storage_secret_access_key: 'MUeY07eEfXVEs6mr8fARQTJYzDaSBS7HiM3MeAfQ'
+    google_storage_access_key_id:     'GOOGFIISYE6YIIOH6R7F',
+    google_storage_secret_access_key: 'f6pBS0d7Hd4mv2btxko+ijTe+LJ0No+AjibEA+fE'
   }
-  config.fog_directory = 'abc_storage'
+  config.fog_directory = 'goostorage'
 end
